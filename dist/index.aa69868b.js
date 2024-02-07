eb7e5762bcf4c5f1c9563469c49c22e91f13a21d
Stina Persson <stpe1901@student.miun.se>
2024-02-07 11:46:12 +0100

    Börjat på hamburgermeny och lagt in lite mer style

---
+++ dist/index.aa69868b.js
@@ -0,0 +1,18 @@
+//För att göra en responsiv hamburgermeny 
+"use strict";
+//läser in element 
+let openButtonEL = document.getElementById("openHamNav");
+let closeButtonEl = document.getElementById("closeHamNav");
+//händelsehanterare 
+openButtonEL.addEventListener("click", toggleMenu);
+closeButtonEl.addEventListener("click", toggleMenu);
+//funktioner 
+function toggleMenu() {
+    //vill att side_nav ska börja synas vid tryck på knappen 
+    let sideNavEl = document.getElementById("hamburgerNav");
+    let style = window.getComputedStyle(sideNavEl);
+    if (style.display === "none") sideNavEl.style.display = "block";
+    else sideNavEl.style.display = "none";
+}
+
+//# sourceMappingURL=index.aa69868b.js.map
