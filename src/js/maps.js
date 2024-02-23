8e7621d694ec016e3f8b21d45472e72e7338b5de
Stina Persson <stpe1901@student.miun.se>
2024-02-23 16:36:53 +0100

    FÅtt ut latidud och longitud ut api, gjort så att man kan söka på plats och den kommer upp på kartan. Kan dock inte söka flera gånger. Behöver fixas.

--- src/js/maps.js
+++ src/js/maps.js
@@ -1,17 +1,5 @@
-"use strict"; 
+"use strict";      
 
-//För kartor 
-var map = L.map('map').setView([58.41080700000001, 15.621372699999938], 13); //Latitud och longitud ska in här för att ändra plats! 
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' //Jag bhöver kanske få in api:n här...? 
-}).addTo(map); 
-
-//API fron nominatim
-let mapURL = "https://nominatim.openstreetmap.org/search?q=17+Strada+Pictor+Alexandru+Romano%2C+Bukarest&format=geojson" // Latitud och longitud härifrån till dit upp!!!!!!!!!!  
-
-window.onload = getMapInfo();  
 
 //För att få knappen att skicka sökvärdet
 function btnFunction() { 
@@ -28,7 +16,8 @@ function inputValue() {
 
     let mapURL = `https://nominatim.openstreetmap.org/search?q=${searchBarValueEl}&format=geojson` 
 
-    getMapInfo(mapURL)
+    getMapInfo(mapURL) 
+
 } 
 
 //hämtar api med sökvärdet 
@@ -38,16 +27,36 @@ async function getMapInfo(mapURL){
 
         let mapInfo = await response.json(); 
 
-        console.table(mapInfo.features); 
+        let lat = mapInfo.features[0].geometry.coordinates[1]; 
+        let long = mapInfo.features[0].geometry.coordinates[0]; 
+
+        createMap(lat, long) 
+
+
     } catch {
         console.log("Något gick galet...")
     }
 } 
 
-// Värdet från Sökbaren ska vara i mitten av api:t. Longituden och latituden av det resultatet ska upp till var map!! 
+//Skapar kartan med hjälp av latitud och longitud för sökningen
+function createMap(lat, long) { 
 
-/*men om man söker på något vanligt kanske det kommer fler resultat? Behöver jag alternativ då? 
-Ska den liveuppdateras?*/
+    if (lat == null && long == null){
+        var map = L.map('map').setView([58.41086, 15.62157], 13);  
+        
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
 
-//'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-//51.505, -0.09
+    } else {
+        var map = L.map('map').setView([lat, long], 13);  
+        
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map); 
+} 
+}
+
+createMap(); //OM KARTAN KALLAS PÅ FÖRST KAN DEN INTE UPPDATERAS SEN. BEHÖVER KUNNA SÖKA FÖERA GÅNGER. 
